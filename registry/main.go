10485c976b31b838582f223e245ac035d4f02b62
Daniel Erez <danielerez@gmail.com>
2025-03-11 11:18:16 +0200

    MGMT-20131: Remove dependency on an external registry image
    Instead of depending on an external registry image
    (pulled from docker.io or quay.io), this change is
    adding logic for building a registry image from:
    github.com/distribution/distribution.
    
    I.e. with this change, we build a registry image
    internally instead of pulling an existing image.
    
    Note: the old behaviour is still supported
    (by specifying a custom image in imageRegistry.uri).

---
+++ registry/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	_ "net/http/pprof"
+
+	"github.com/distribution/distribution/v3/registry"
+	_ "github.com/distribution/distribution/v3/registry/auth/htpasswd"
+	_ "github.com/distribution/distribution/v3/registry/auth/silly"
+	_ "github.com/distribution/distribution/v3/registry/auth/token"
+	_ "github.com/distribution/distribution/v3/registry/proxy"
+	_ "github.com/distribution/distribution/v3/registry/storage/driver/filesystem"
+)
+
+func main() {
+	// nolint:errcheck
+	registry.RootCmd.Execute()
+}
